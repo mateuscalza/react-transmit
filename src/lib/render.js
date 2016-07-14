dcb420fbbfbd541b21c2feb078013bb0dba855b4
Mateus Calza <mateushenrique17@gmail.com>
2016-07-13 21:32:38 -0300

    React-router custom createElement support.

--- src/lib/render.js
+++ src/lib/render.js
@@ -1,33 +1,21 @@
 /**
  * @copyright © 2015, Rick Wong. All rights reserved.
- */
-"use strict";
-
-var assign                = require("./assign");
-var isRootContainer       = require("./isRootContainer");
-var overrideCreateElement = require("./overrideCreateElement");
-var React                 = require("./react");
-var ReactDOM              = require("./react-dom");
-var takeFromMarkup        = require("./takeFromMarkup");
-
-var reactData = takeFromMarkup();
-
-/**
+ */ "use strict"; var assign = require("./assign"); var isRootContainer = require("./isRootContainer"); var overrideCreateElement = require("./overrideCreateElement"); var React = 
+require("./react"); var ReactDOM = require("./react-dom"); var takeFromMarkup = require("./takeFromMarkup"); var reactData = takeFromMarkup(); /**
  * @function render
- */
-module.exports = function (Component, props, targetDOMNode, callback) {
+ */ module.exports = function render(Component, props, targetDOMNode, callback) {
 	var fetchedFragments = reactData;
-
+	var replacement = function (originalCreateElement, type, props, children) {
+		var args = [].slice.call(arguments, 1);
+		if (isRootContainer(type) && fetchedFragments.length) {
+			assign(props, fetchedFragments.pop());
+		}
+		return originalCreateElement.apply(null, args);
+	};
+	render.reactCreateElementReplacement = replacement;
+	
 	overrideCreateElement(
-		function (originalCreateElement, type, props, children) {
-			var args = [].slice.call(arguments, 1);
-
-			if (isRootContainer(type) && fetchedFragments.length) {
-				assign(props, fetchedFragments.pop());
-			}
-
-			return originalCreateElement.apply(null, args);
-		},
+		replacement,
 		function () {
 			assign(props, {createElement: React.createElement});
 			ReactDOM.render(React.createElement(Component, props), targetDOMNode, callback);
